a981d2f07502ea49632a872e2368fde81303feca
AbdulKareem-M <abdulkareemyousaf1245@gmail.com>
2025-02-11 21:46:30 +0530

    created a model TaskModel

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TaskModel(models.Model):
+  title = models.CharField(max_length=100)
+  completed = models.BooleanField(default=False)
+  created_at = models.DateField(auto_now_add=True)
+  
+  def __str__(self):
+    return self.title
