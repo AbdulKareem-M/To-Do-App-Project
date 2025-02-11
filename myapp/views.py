9224693daf584014afe7fcc280f860a0f03d4dfe
AbdulKareem-M <abdulkareemyousaf1245@gmail.com>
2025-02-11 23:18:12 +0530

    added views for add,list, delete and update of tasks

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import TaskModel
 
-# Create your views here.
+# List View(Read)
+class TaskList(ListView):
+  model = TaskModel
+  template_name = 'index.html'
+  context_object_name = 'tasks'
+
+
+# Create View(add)
+class TaskAdd(CreateView):
+  model = TaskModel
+  fields = ['title', 'completed']
+  template_name = 'taskform.html'
+  success_url = reverse_lazy('index')
+  
+# Update View (Edit)
+class TaskUpdateView(UpdateView):
+    model = TaskModel
+    fields = ['title', 'completed']
+    template_name = 'taskupdate.html'
+    success_url = reverse_lazy('index')
+
+# Delete View (Delete)
+class TaskDeleteView(DeleteView):
+    model = TaskModel
+    template_name = 'confirm_delete.html'
+    success_url = reverse_lazy('index')
